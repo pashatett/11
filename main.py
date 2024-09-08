f30cd2f2dd29a20eed112b4a557397df0fa586f5
dweavp <100860878+pashatett@users.noreply.github.com>
2024-09-08 09:40:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 my_list = [42,69,322,13,0,99,-5,9,8,7,-6,5]
 k = 0
-while my_list[k]>=0 and k<len(my_list):
-    if  my_list[k] > 0:
+while k<len(my_list):
+    if my_list[k] < 0:
+        break
+    elif my_list[k] > 0:
         print(my_list[k])
     k += 1
+    if my_list[k] == 0:
+        continue
